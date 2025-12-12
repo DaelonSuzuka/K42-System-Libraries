bc9106e4cb6a51d4bf618cff844c75bdd7dacf1b
David Kincaid <daelonsuzuka@gmail.com>
2025-12-12 12:07:59 -0500

    Add types.h

---
+++ types.h
@@ -0,0 +1,24 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <stdint.h>
+
+/* ************************************************************************** */
+
+typedef uint8_t byte;
+
+// integer types
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint24_t u24;
+typedef int24_t s24;
+typedef uint32_t u32;
+typedef int32_t s32;
+
+// floating point types
+typedef float f24;
+typedef double f32;
+
+#endif // _TYPES_H_
